8ae9b3a6b01c1b37622ce97980aa6330b4068af4
Amirhossein Zareei <amirhossein2004zareei@gmail.com>
2025-05-22 08:35:15 +0330

    Feat: add Order and OrderItem models.

---
+++ src/models/Order.js
@@ -0,0 +1,78 @@
+const { Schema, model } = require("mongoose");
+
+const statusHistorySchema = new Schema(
+  {
+    status: {
+      type: String,
+      enum: ["pending", "approved", "rejected", "shipped", "delivered"],
+      default: "pending",
+      required: true,
+    },
+
+    timestamp: {
+      type: Date,
+      default: Date.now,
+      required: true,
+    },
+
+    note: {
+      type: String,
+      default: "",
+      trim: true,
+      required: false,
+    },
+  },
+  { _id: false }
+);
+
+const orderSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    items: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "OrderItem",
+        required: true,
+      },
+    ],
+
+    totalAmount: {
+      type: Number,
+      min: 0,
+      required: true,
+    },
+
+    statusHistory: [statusHistorySchema],
+
+    shippingAddress: {
+      type: Schema.Types.ObjectId,
+      ref: "Address",
+      required: true,
+    },
+
+    paymentMethod: {
+      type: String,
+      enum: ["stripe", "paypal"],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+orderSchema.virtual("currentStatus").get(function () {
+  const status = this.statusHistory;
+
+  return status[status.length - 1].status;
+});
+
+orderSchema.set("toJSON", { virtuals: true });
+orderSchema.set("toObject", { virtuals: true });
+
+orderSchema.index({ userId: 1 });
+
+module.exports = model("Order", orderSchema);
