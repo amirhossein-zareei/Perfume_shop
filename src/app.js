f4870a5f3a95cb6e094b064ef6ae688c5df21449
Amirhossein Zareei <amirhossein2004zareei@gmail.com>
2025-05-22 13:51:46 +0330

    feat: add global error handler and response utility functions.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 
+const errorHandler = require("./middlewares/errorHandler");
+
 const app = express();
 
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
+
+app.use(errorHandler)
 
 module.exports = app;
