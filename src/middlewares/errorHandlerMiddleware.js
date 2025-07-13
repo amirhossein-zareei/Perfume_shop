b1d35657f27dd2fcd75293b655d03e5aef7ed6be
Amirhossein Zareei <amirhossein2004zareei@gmail.com>
2025-07-13 10:49:12 +0330

    Feat: enhance error handling middleware to delete orphan files after upload errors.

--- src/middlewares/errorHandlerMiddleware.js
+++ src/middlewares/errorHandlerMiddleware.js
@@ -1,7 +1,31 @@
 const logger = require("../utils/logger");
 const { app } = require("../config/env");
 
-module.exports = (err, req, res, next) => {
+const { deleteFiles } = require("../services/cloudinaryService");
+
+module.exports = async (err, req, res, next) => {
+  if (req.file) {
+    const publicId = req.file.filename;
+
+    logger.warn(
+      `An error occurred after file upload. Deleting orphan file: ${publicId}`
+    );
+
+    await deleteFiles(publicId);
+  }
+
+  if (req.files) {
+    const publicIds = req.files.map((file) => file.filename);
+
+    logger.warn(
+      `An error occurred after multiple file uploads. Deleting orphan files: ${publicIds.join(
+        ", "
+      )}`
+    );
+
+    await deleteFiles(publicIds);
+  }
+
   const isProduction = app.mode === "production";
 
   const status = err.status || "error";
@@ -33,5 +57,5 @@ module.exports = (err, req, res, next) => {
     response.stack = err.stack;
   }
 
- return res.status(statusCode).json(response);
+  return res.status(statusCode).json(response);
 };
